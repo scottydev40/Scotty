TheShepord/nearby

[fp-rs] Device now holds 16bit UUID service data, collected by Adapter.

fastpair/rust/src/bluetooth/common/data.rs
@@ -12,13 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// Module for shared functionality between all Bluetooth platforms.
-mod adapter;
-mod address;
-mod data;
-mod device;
+pub enum BleDataSection {
+    ServiceData16BitUUid = 0x16,
+}
 
-pub use adapter::*;
-pub use address::*;
-pub use data::*;
-pub use device::*;
+pub struct ServiceData<Uuid: Copy> {
+    uuid: Uuid,
+    data: Vec<u8>,
+}
+
+impl<Uuid: Copy> ServiceData<Uuid> {
+    pub fn new(uuid: Uuid, data: Vec<u8>) -> Self {
+        ServiceData { uuid, data }
+    }
+
+    pub fn get_uuid(&self) -> Uuid {
+        self.uuid
+    }
+
+    pub fn get_data(&self) -> &Vec<u8> {
+        &self.data
+    }
+}

fastpair/rust/src/bluetooth/windows/adapter.rs
@@ -51,10 +51,15 @@ use windows::{
     // (e.g. Received and Stopped events in BluetoothLEAdvertisementWatcher).
     // https://learn.microsoft.com/en-us/uwp/api/windows.foundation.typedeventhandler-2?view=winrt-22621
     Foundation::TypedEventHandler,
+
+    // TODO
+    Storage::Streams::DataReader,
 };
 
 use super::BleDevice;
-use crate::bluetooth::common::{Adapter, BleAddress, BleAddressKind};
+use crate::bluetooth::common::{
+    Adapter, BleAddress, BleAddressKind, BleDataSection, ServiceData,
+};
 
 /// Struct holding the necessary fields for listening to and handling incoming
 /// BLE advertisements.
@@ -69,6 +74,44 @@ struct AdListener {
 pub struct BleAdapter {
     inner: BluetoothAdapter,
     listener: Option<AdListener>,
+}
+
+/// Parse the advertisement's service data.
+/// Further Reading:
+/// * `BleMedium::AdvertisementReceivedHandler` under
+/// github.com/google/nearby/internal/platform/implementation/windows_ble/ble_medium.cc.
+/// * Bluetooth Core Specification Supplement, Part A, Section 1.11.
+/// * go/fast_pair_windows_data_parse.
+#[inline]
+fn get_service_data_16bit_uuid(
+    event_args: &BluetoothLEAdvertisementReceivedEventArgs,
+) -> Result<Vec<ServiceData<u16>>, anyhow::Error> {
+    let advertisement = event_args.Advertisement()?;
+    let mut service_data_vec = Vec::new();
+
+    // Note `service_data` is `!Send` and `!Sync`. This means processing must
+    // occur in a synchronous environment (namely, this function's scope).
+    // The compiler will complain if similar code is written between awaits
+    // in an async function.
+    for service_data in advertisement
+        .GetSectionsByType(BleDataSection::ServiceData16BitUUid as u8)?
+    {
+        let data_reader = DataReader::FromBuffer(&service_data.Data()?)?;
+
+        let first_byte = data_reader.ReadByte()?;
+        let second_byte = data_reader.ReadByte()?;
+        let uuid = ((first_byte as u16) << 8) | (second_byte as u16);
+
+        let unconsumed_buffer_len = data_reader.UnconsumedBufferLength()?;
+        let mut data = Vec::new();
+
+        for _ in 0..unconsumed_buffer_len {
+            data.push(data_reader.ReadByte()?);
+        }
+
+        service_data_vec.push(ServiceData::new(uuid, data));
+    }
+    Ok(service_data_vec)
 }
 
 #[async_trait]
@@ -197,13 +240,12 @@ impl Adapter for BleAdapter {
 
                         let kind = BleAddressKind::try_from(kind)?;
                         let addr = BleAddress::new(addr, kind);
+                        let service_data =
+                            get_service_data_16bit_uuid(&event_args)?;
 
-                        match BleDevice::new(addr).await {
-                            Ok(device) => break Ok(device),
-                            Err(err) => {
-                                warn!("Error creating device: {:?}", err);
-                            }
-                        }
+                        let device = BleDevice::new(addr, service_data).await?;
+
+                        break Ok(device);
                     }
                 }
             }

scottydev40/Scotty

fastpair/rust/src/bluetooth/common/data.rs
@@ -12,15 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// Module for shared functionality between all Bluetooth platforms.
-mod adapter;
-mod address;
-mod data;
-mod device;
-mod error;
+pub enum BleDataSection {
+    ServiceData16BitUUid = 0x16,
+}
 
-pub use adapter::*;
-pub use address::*;
-pub use data::*;
-pub use device::*;
-pub use error::*;
+pub struct ServiceData<U: Copy> {
+    uuid: U,
+    data: Vec<u8>,
+}
+
+impl<U: Copy> ServiceData<U> {
+    pub fn new(uuid: U, data: Vec<u8>) -> Self {
+        ServiceData { uuid, data }
+    }
+
+    pub fn uuid(&self) -> U {
+        self.uuid
+    }
+
+    pub fn data(&self) -> &Vec<u8> {
+        &self.data
+    }
+}

fastpair/rust/src/bluetooth/windows/adapter.rs
@@ -51,11 +51,16 @@ use windows::{
     // (e.g. Received and Stopped events in BluetoothLEAdvertisementWatcher).
     // https://learn.microsoft.com/en-us/uwp/api/windows.foundation.typedeventhandler-2?view=winrt-22621
     Foundation::TypedEventHandler,
+
+    // Struct for reading data from a Windows stream, like an IVectorView.
+    // https://learn.microsoft.com/en-us/uwp/api/windows.storage.streams.datareader?view=winrt-22621
+    Storage::Streams::DataReader,
 };
 
 use super::BleDevice;
 use crate::bluetooth::common::{
-    Adapter, BleAddress, BleAddressKind, BluetoothError,
+    Adapter, BleAddress, BleAddressKind, BleDataSection, BluetoothError,
+    ServiceData,
 };
 
 /// Struct holding the necessary fields for listening to and handling incoming
@@ -71,6 +76,39 @@ struct AdvListener {
 pub struct BleAdapter {
     inner: BluetoothAdapter,
     listener: Option<AdvListener>,
+}
+
+/// Parse the advertisement's service data.
+/// Further Reading:
+/// * `BleMedium::AdvertisementReceivedHandler` under
+/// github.com/google/nearby/internal/platform/implementation/windows_ble/ble_medium.cc.
+/// * Bluetooth Core Specification Supplement, Part A, Section 1.11.
+/// * go/fast_pair_windows_data_parse.
+#[inline]
+fn get_service_data_16bit_uuid(
+    event_args: &BluetoothLEAdvertisementReceivedEventArgs,
+) -> Result<Vec<ServiceData<u16>>, BluetoothError> {
+    let advertisement = event_args.Advertisement()?;
+    let mut service_data_vec = Vec::new();
+
+    // Note `service_data` is `!Send` and `!Sync`. This means processing must
+    // occur in a synchronous environment (namely, this function's scope).
+    // The compiler will complain if similar code is written between awaits
+    // in an async function.
+    for service_data in advertisement
+        .GetSectionsByType(BleDataSection::ServiceData16BitUUid as u8)?
+    {
+        let data_reader = DataReader::FromBuffer(&service_data.Data()?)?;
+        let uuid = data_reader.ReadUInt16()?;
+
+        let unconsumed_buffer_len =
+            data_reader.UnconsumedBufferLength()? as usize;
+        let mut data = vec![0u8; unconsumed_buffer_len];
+        data_reader.ReadBytes(&mut data)?;
+
+        service_data_vec.push(ServiceData::new(uuid, data));
+    }
+    Ok(service_data_vec)
 }
 
 #[async_trait]
@@ -202,13 +240,12 @@ impl Adapter for BleAdapter {
 
                         let kind = BleAddressKind::try_from(kind)?;
                         let addr = BleAddress::new(addr, kind);
+                        let service_data =
+                            get_service_data_16bit_uuid(&event_args)?;
 
-                        match BleDevice::new(addr).await {
-                            Ok(device) => break Ok(device),
-                            Err(err) => {
-                                warn!("Error creating device: {:?}", err);
-                            }
-                        }
+                        let device = BleDevice::new(addr, service_data).await?;
+
+                        break Ok(device);
                     }
                 }
             }