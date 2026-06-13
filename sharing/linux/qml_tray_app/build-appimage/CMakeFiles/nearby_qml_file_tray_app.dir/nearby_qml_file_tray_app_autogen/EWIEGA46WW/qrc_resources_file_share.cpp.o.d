8002d0e5def8b148c40afb524f90bf53b1c175da
Lasan Mahaliyana <limahaliyana@gmail.com>
2026-06-13 22:10:38 +0530

    Stop tracking unnecessary build files

--- sharing/linux/qml_tray_app/build-appimage/CMakeFiles/nearby_qml_file_tray_app.dir/nearby_qml_file_tray_app_autogen/EWIEGA46WW/qrc_resources_file_share.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/nearby_qml_file_tray_app.dir/nearby_qml_file_tray_app_autogen/EWIEGA46WW/qrc_resources_file_share.cpp.o: \
- /home/lasan/Dev/nearby_latest/sharing/linux/qml_tray_app/build-appimage/nearby_qml_file_tray_app_autogen/EWIEGA46WW/qrc_resources_file_share.cpp \
- /usr/include/stdc-predef.h
